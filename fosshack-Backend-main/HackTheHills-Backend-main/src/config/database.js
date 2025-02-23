93a87f83abb73e5ddfafcf30d468e3492750d3ce
Aditya Godara <45264723+AdityaGodara@users.noreply.github.com>
2025-02-23 14:05:46 +0530

    Delete fosshack-Backend-main/HackTheHills-Backend-main directory

--- fosshack-Backend-main/HackTheHills-Backend-main/src/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose= require("mongoose");
-
-const connectDB= async()=>{
-    await mongoose.connect("mongodb+srv://LAD_PROJECT:LAD%40123@ladproject.cfgyg.mongodb.net/")
-}
-
-module.exports=connectDB
